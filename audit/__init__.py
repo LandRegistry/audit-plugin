8db7c83eac381f6719807af06c238e0ebbe14342
Juan Matthys Uys <opyate@gmail.com>
2014-08-05 20:53:43 +0100

    cleanup

--- audit/__init__.py
+++ audit/__init__.py
@@ -1,27 +1,32 @@
-from flask import request_started, session
+from flask import request_started, request, session
+
+
+TEMPLATE = 'Audit: user=[%s], request=[%s], session=[%s]'
 
 
 class Audit(object):
-
-    LOG_TEMPLATE = 'Audit: user=[%s], request=[%s], session=[%s]'
 
     def __init__(self, app):
         try:
             # check if the client has Flask-Login
             from flask.ext.login import current_user
-            request_started.connect(self.audit, app)
+            request_started.connect(audit_user, app)
         except:
-            request_started.connect(self.anon_audit, app)
- 
-    def anon_audit(sender, **extra):
-        sender.logger.info(TEMPLATE % ('anon', request, session))
-
-    def audit(sender, **extra):
-        id = current_user.get_id()
-        if id:
-            sender.logger.info(TEMPLATE % (current_user, request, session))
-        else:
-            sender.logger.info(TEMPLATE % ('anon', request, session))
+            request_started.connect(audit_anon, app)
 
 
-       
+def audit_user(sender, **extra):
+    from flask.ext.login import current_user
+    id = current_user.get_id()
+    if id:
+        log(sender, current_user, request, session)
+    else:
+        log(sender, 'anon', request, session)
+
+
+def audit_anon(sender, **extra):
+    log(sender, 'anon', request, session)
+
+
+def log(sender, who, request, session):
+    sender.logger.info(TEMPLATE % (who, request, session))
